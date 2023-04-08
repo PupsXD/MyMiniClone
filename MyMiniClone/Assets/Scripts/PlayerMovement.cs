2993d05da2f140f5ba49d0e9b609671f89f4ecb6
PupsXD <UnusialFish@yandex.ru>
2023-04-08 03:04:35 +0300

    MovementSystem_TriggerScript

---
+++ MyMiniClone/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f; // The movement speed of the player
+
+    private Rigidbody rb; // CHANGE THAT TO PUBLIC LATER
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal"); /
+        float vertical = Input.GetAxis("Vertical"); 
+
+        Vector3 movement = new Vector3(horizontal, 0.0f, vertical); // Create a movement vector based on the input
+        Vector3 newPosition = transform.position + movement * speed * Time.deltaTime; // Calculate the new position
+
+        rb.MovePosition(newPosition); // Move the player using the MovePosition method of the Rigidbody component
+    }
+}
---
+++ MyMiniClone/Assets/Scripts/TriggerButtonUnlock.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerButtonUnlock : MonoBehaviour
+{
+    public Button buttonToUnlock;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            buttonToUnlock.interactable = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            buttonToUnlock.interactable = false;
+        }
+    }
+}
